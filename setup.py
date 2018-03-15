e3b91c2faed5debc42a7a87c40539ab20a22d4cb
Josh Hall <josh@sobo.red>
2018-03-15 17:47:26 -0500

    Remove bumpversion until fully released.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykit',
-    version='0.0.1',
+    version='1.0.0-dev',
     description='IBM i toolkit',
     url='https://github.com/Club-Seiden/pykit',
     author="Joshua Hall, Stephanie Rabbini",
