f0db4fe48071e51acdfe6d63808cc4f776fbcbdc
Josh Hall <josh@sobo.red>
2018-03-15 16:48:35 -0500

    Add methods for executing HTTP Connection and their tests

--- pykit/transport/http/HttpConnection.py
+++ pykit/transport/http/HttpConnection.py
@@ -1,4 +1,5 @@
 import requests
+from ...Toolkit import Toolkit
 
 
 class HttpConnection:
@@ -8,21 +9,55 @@ class HttpConnection:
     """
 
     def __init__(self, db2sock_rest_url, db2sock_auth):
-        """
-        Test that the DB2Sock connection exists
-        """
         self.db2sock_auth = db2sock_auth
         self.db2sock_rest_url = db2sock_rest_url
+        self.payload = []
+        self.test_db2sock()
 
-        payload = {
+    def toolkit(self):
+        """
+        Return an instance of the toolkit with a connection defined.
+
+        :return: Toolkit
+        """
+        return Toolkit(self)
+
+    def add(self, o):
+        """
+        Add an object to the payload that will be passed to DB2Sock.
+
+        :param o: Object to be added
+        :return: void
+        """
+        if isinstance(o, dict):
+            self.payload.append(o)
+        else:
+            raise TypeError('Only dictionaries are supported right now.')
+
+    def execute(self):
+        """
+        Execute the payload and then clear the payload.
+
+        :return: Response
+        """
+        response = requests.post(
+            self.db2sock_rest_url, json=self.payload, auth=self.db2sock_auth)
+        self.payload = []
+        return response
+
+    def test_db2sock(self):
+        """
+        Test that the DB2Sock REST transport exists and works properly
+        with the given configuration. Raise an error if not.
+
+        :return: void
+        """
+        self.add({
             'pgm': [
                 {'name': 'HELLO', 'lib': 'DB2JSON'},
                 {'s': {'name': 'char', 'type': '128a', 'value': 'Hi there'}}
             ]
-        }
-
-        response = requests.post(
-            self.db2sock_rest_url, json=payload, auth=self.db2sock_auth)
-
+        })
+        response = self.execute()
         if not response.ok:
             raise ConnectionError("DB2Sock REST Transport failed to connect.")
--- pykit/transport/http/test_http.py
+++ pykit/transport/http/test_http.py
@@ -10,14 +10,19 @@ class TestHttpConnection(unittest.TestCase):
             db2sock_auth=(
                 os.environ['PK_DB2SOCK_USER'], os.environ['PK_DB2SOCK_PASS']))
 
-    def test_url_is_set(self):
-        self.assertEqual(
-            self.connection.db2sock_rest_url, os.environ['PK_DB2SOCK_URL'])
+    def test_add_payload(self):
+        payload = {
+            'pgm': [
+                {'name': 'HELLO', 'lib': 'DB2JSON'},
+                {'s': {'name': 'char', 'type': '128a', 'value': 'Hi there'}}
+            ]
+        }
+        self.connection.add(payload)
+        self.assertIn(payload, self.connection.payload)
 
-    def test_auth_is_set(self):
-        self.assertEqual(
-            self.connection.db2sock_auth,
-            (os.environ['PK_DB2SOCK_USER'], os.environ['PK_DB2SOCK_PASS']))
+    def test_execute_payload(self):
+        response = self.connection.execute()
+        self.assertEqual(response.ok, True)
 
 
 if __name__ == '__main__':
