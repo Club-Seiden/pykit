baa9efc12328c84c5345d3d50526eea7f4068223
Josh Hall <josh@sobo.red>
2018-03-15 17:24:09 -0500

    Add Toolkit

---
+++ pykit/Toolkit.py
@@ -0,0 +1,27 @@
+import requests
+
+
+class Toolkit:
+    """
+    Python Toolkit for interacting with the IBM i via different transports.
+    """
+    def __init__(self, connection):
+        self.connection = connection
+        self.payload = ''
+
+    def add(self, o):
+        """
+        Add object to the transport's payload.
+
+        :param o: Object to be added to the transport
+        :return: void
+        """
+        self.connection.add(o)
+
+    def execute(self):
+        """
+        Execute the transport's payload.
+
+        :return: void
+        """
+        return self.connection.execute()
---
+++ pykit/test_toolkit.py
@@ -0,0 +1,31 @@
+import unittest
+import os
+from .transport.http import connect
+
+
+class TestHttpConnection(unittest.TestCase):
+    def setUp(self):
+        self.connection = connect(
+            os.environ['PK_DB2SOCK_URL'],
+            db2sock_auth=(
+                os.environ['PK_DB2SOCK_USER'], os.environ['PK_DB2SOCK_PASS']))
+        self.toolkit = self.connection.toolkit()
+
+    def test_add_payload(self):
+        orig_size = len(self.connection.payload)
+        payload = {
+            'pgm': [
+                {'name': 'HELLO', 'lib': 'DB2JSON'},
+                {'s': {'name': 'char', 'type': '128a', 'value': 'Hi there'}}
+            ]
+        }
+        self.toolkit.add(payload)
+        self.assertTrue(len(self.connection.payload) - orig_size == 1)
+
+    def test_execute_payload(self):
+        response = self.toolkit.execute()
+        self.assertEqual(response.ok, True)
+
+
+if __name__ == '__main__':
+    unittest.main()
