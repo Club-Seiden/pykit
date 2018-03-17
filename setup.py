5a7635616ff8002bfba155e47313f40e81a6b7ae
Josh Hall <josh@sobo.red>
2018-03-16 20:30:18 -0500

    Fix version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pykit',
-    version='1.0.0-dev',
+    version='0.1.0',
     description='IBM i toolkit',
     url='https://github.com/Club-Seiden/pykit',
     author="Joshua Hall, Stephanie Rabbini",
