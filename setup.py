1354e6b062ee9ce4d83813cb6026039af038bb33
Josh Hall <josh@sobo.red>
2018-03-27 12:23:12 -0500

    Fix typo

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     description='IBM i toolkit',
     url='https://github.com/Club-Seiden/pykit',
-    author="Joshua Hall, Stephanie Rabbini",
+    author="Joshua Hall, Stephanie Rabbani",
     author_email='josh@seidengroup.com, steph@seidengroup.com',
     license='MIT',
     packages=find_packages(),
