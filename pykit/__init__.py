c100b786eeefe72469a6932a8eac2bf6d4ca8683
jordiwes <jordiwes@gmail.com>
2018-07-24 12:24:32 -0700

    improt types

--- pykit/__init__.py
+++ pykit/__init__.py
@@ -3,5 +3,7 @@ from .ClCommand import ClCommand
 from .types import Integer
 from .types import Float
 from .types import Character
+from .types import Decimal
+from .types import Binary
 from .program import Program
 from .program import ServiceProgram
