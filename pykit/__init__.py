8e5991ed0266416fc3d48cc2ff24321ad89981b6
jordiwes <jordiwes@gmail.com>
2018-07-23 17:05:02 -0700

    updated init

--- pykit/__init__.py
+++ pykit/__init__.py
@@ -1,7 +1,7 @@
 __version__ = "0.2.0"
-from .ClCommand import ClCommand
-from .Integer import Integer
-from .Float import Float
-from .Character import Character
+from .types import ClCommand
+from .types import Integer
+from .types import Float
+from .types import Character
 from .Program import Program
 from .ServiceProgram import ServiceProgram
