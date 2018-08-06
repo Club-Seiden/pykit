1d7ae73333c96a0890e78aedaaacd0f69a3fab51
jordiwes <jordiwes@gmail.com>
2018-08-06 11:35:07 -0700

    fix init

--- pykit/transport/db2/__init__.py
+++ pykit/transport/db2/__init__.py
@@ -1,5 +1,5 @@
 from .Db2Connection import Db2Connection
 
 
-def connect(database, username, password):
+def connect(database='*LOCAL', username=None, password=None):
     return Db2Connection(database, username, password)
